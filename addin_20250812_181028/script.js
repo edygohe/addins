04a3db5cf3cfe5ecc35213226dac61e889a7f3ad
Edgar <edygohe@gmail.com>
2025-08-13 08:20:52 -0600

    Se depuran directorios

--- addin_20250812_181028/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Delegación de eventos para manejar la interactividad
-    document.getElementById('geotabAddin').addEventListener('click', function(event) {
-        if (event.target && event.target.id === 'mostrarMensaje') {
-            alert('Hola mundo Geotab Génesis');
-        }
-    });
-});
